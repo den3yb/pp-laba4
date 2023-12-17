a14884207135a6b9dc493c2da0f0749226a0a05a
den3yb <den3yb@gmail.com>
2023-12-18 00:09:08 +0400

    После настройки графиков и стоп слов

--- functions.py
+++ functions.py
@@ -1,6 +1,15 @@
 import pandas 
 import string
 import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+from nltk.probability import FreqDist
+from nltk.stem.snowball import SnowballStemmer
+from pymystem3 import Mystem
+import matplotlib.pyplot
+
+
 
 
 def create_ann(annatation: str) -> pandas.DataFrame:
@@ -73,6 +82,51 @@ def word_stat (frame: pandas.DataFrame) -> pandas.DataFrame:
     temp["mid"]=mid_word
     return(temp)
     
+def create_series (frame: pandas.DataFrame) -> pandas.Series:
+    strings = to_str(frame)
+    strings = delete_marks(strings)
+    strings = to_lemm(strings)
+    dictionary = to_dict(strings)
+    d_series = pandas.Series(dictionary)
+    return(d_series)
     
     
-    
+def to_str(frame: pandas.DataFrame) -> str:
+    lists = " "
+    for i in frame["Текст рецензии"]:
+        lists += i + " "
+    return(lists.lower())
+
+def delete_marks (strings: str) -> str:
+    punc = '''!()-[]{};:'",<>./?@#$%^&*~1234567890'''
+    for char in strings:
+        if char in punc:
+            strings = strings.replace(char, " ")
+    return strings
+
+def to_lemm (strings: str) -> str:
+    temp = Mystem().lemmatize(strings)
+    to_s = " ".join(temp)
+    return to_s
+
+def to_dict (strings: str) -> dict:
+    stop_words = stopwords.words('russian')
+    stop_words += ['банк','карта','счёт','сбербанк','перевод','перевести','сбербанк','счет','деньги','это','весь','который','кредит','сбер','банкомат']
+    temp = word_tokenize(strings)
+    filtred = [word for word in temp if word not in stop_words]
+    dictionary = {}
+    for word in filtred:
+        if word in dictionary:
+            dictionary[word] +=1
+        else:
+            dictionary[word] = 1
+    return (dictionary)
+
+def create_graph(ser: pandas.Series) -> None:
+
+    ser = ser.nlargest(20)
+    matplotlib.pyplot.barh(ser.index, ser.values)
+    matplotlib.pyplot.xlabel("Слова")
+    matplotlib.pyplot.ylabel("Количество")
+    matplotlib.pyplot.title("Анализ")
+    matplotlib.pyplot.show()
--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from functions import *
 from IPython.display import display
 
+
+
 if __name__ == "__main__":
     data = create_ann("C:\\Proganiy\\pp-laba3\\annotation.csv")
-    stat = word_stat(data)
-    data = sort_word(data)
-    display(stat)
+    serieses = create_series(data)
+    create_graph(serieses)
+
+    
